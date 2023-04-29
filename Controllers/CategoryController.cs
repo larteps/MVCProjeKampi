72ae8e52fa6a275b79a0f7126f5fb87faebb6da3
larisateps <larisateps76@gmail.com>
2023-04-29 14:45:34 +0300

    TEST

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using BuisnessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace MVCProjeKampi.Controllers
 {
     public class CategoryController : Controller
-    {
-        // GET: Category
+    {        
+        CategoryManager cm = new CategoryManager();
+
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetList();
+            return View (categoryvalues);
+        }
+
+        [HttpGet] public ActionResult GetCategory(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            cm.AddCategory(p);
+            return RedirectToAction("GetCategoryList");
+        }
+        
     }
 }
